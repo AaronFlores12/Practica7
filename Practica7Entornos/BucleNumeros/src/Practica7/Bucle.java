9815d7f7fc8da88798ed237b06a870af827aeb3e
Aaron Flores Sanchez <aaronfloressanchez12@gmail.com>
2023-12-20 22:56:45 +0100

    Bucle de 31 numeros

--- Practica7Entornos/BucleNumeros/src/Practica7/Bucle.java
+++ Practica7Entornos/BucleNumeros/src/Practica7/Bucle.java
@@ -2,7 +2,7 @@ package Practica7;
 
 public class Bucle {
 	public static void main(String[] args) {
-		for (int i = 0; i < 21; i++) {
+		for (int i = 0; i < 31; i++) {
 			System.out.print((i)+" ");
 		}
 	}
